fd5074f97fbeed6744cef51985b0cae6fb1a4526
FullStackDj <full.stackdj@gmail.com>
2025-04-12 19:02:32 +0300

    Refactored template methods in the base class

--- core/user/controller/IndexController.php
+++ core/user/controller/IndexController.php
@@ -5,7 +5,19 @@ namespace core\user\controller;
 use core\base\controller\BaseController;
 
 class IndexController extends BaseController {
+
+    protected $name;
+
     protected function inputData() {
 
+        $name = 'test2';
+
+        $content = $this->render('', compact('name'));
+        $header = $this->render(TEMPLATE . 'header');
+        $footer = $this->render(TEMPLATE . 'footer');
+
+        return compact('header', 'content', 'footer');
     }
+
+
 }
