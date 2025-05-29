e146c10ef61ab6fee487e7b41786eb364b576211
FullStackDj <full.stackdj@gmail.com>
2025-05-29 16:10:50 +0300

    Implemented a data editing controller for the admin panel

--- core/admin/controller/ShowController.php
+++ core/admin/controller/ShowController.php
@@ -9,13 +9,13 @@ class ShowController extends BaseAdmin {
 
     protected function inputData() {
 
-        $this->execBase();
+        if(!$this->userId) $this->execBase();
 
         $this->createTableData();
 
         $this->createData();
 
-        return $this->expansion(get_defined_vars());
+        return $this->expansion();
     }
 
     protected function createData($arr = []) {
